40536a2185e628c6040514cc0408baaf5be9dc94
batako <guri725ren213@gmail.com>
2020-10-18 12:49:12 +0900

    モジュールで作成したオブジェクトのIDを出力するように

--- module/network/simple/outputs.tf
+++ module/network/simple/outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
   value = aws_vpc.template.id
 }
+
+output "subnet_public_id" {
+  value = module.public_subnet.id
+}
+
+output "subnet_private_id" {
+  value = module.private_subnet.id
+}
